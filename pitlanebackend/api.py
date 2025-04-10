778e0ba9cda0105375b3493fb00f27ec7ed0d97e
Justin Becker <justin.becker1@gmx.de>
2025-04-10 21:54:22 +0200

    first api basic implementation

---
+++ pitlanebackend/api.py
@@ -0,0 +1,28 @@
+from ninja import NinjaAPI
+from ninja import Schema
+from ninja.responses import Response
+from django.contrib.auth.models import User
+from typing import List
+
+api = NinjaAPI()
+
+class UserOut(Schema):
+    id:int
+    username:str
+    email:str
+
+@api.get("/user", response=List[UserOut])
+def get_all_users(request):
+    users = User.objects.all()
+    return users
+
+@api.get("/user/{user_id}", response=UserOut)
+def get_single_user(request, user_id: int):
+    try:
+        user = User.objects.get(id=user_id)
+        return user
+    except User.DoesNotExist:
+        return Response(
+            {"detail": f"User with id {user_id} not found"},
+            status=404
+        )
