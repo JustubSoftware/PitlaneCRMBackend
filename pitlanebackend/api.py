e6533f792b73fc8ca70506e8fd7ec3e7f9556bd9
Justin Becker <justin.becker1@gmx.de>
2025-04-13 11:24:11 +0200

    update for frontned

--- pitlanebackend/api.py
+++ pitlanebackend/api.py
@@ -1,8 +1,8 @@
+from typing import Optional, List
 from ninja import NinjaAPI
 from ninja import Schema
 from ninja.responses import Response
 from django.contrib.auth.models import User
-from typing import List
 
 api = NinjaAPI()
 
@@ -12,6 +12,13 @@ class UserOut(Schema):
     last_name:str
     username:str
     email:str
+
+class UserCreate(Schema):
+    username:str
+    email:str
+    password:str
+    first_name:Optional[str] = ""
+    last_name:Optional[str] = ""
 
 @api.get("/user", response=List[UserOut])
 def get_all_users(request):
@@ -28,3 +35,18 @@ def get_single_user(request, user_id: int):
             {"detail": f"User with id {user_id} not found"},
             status=404
         )
+
+@api.post("/user", response={201: UserOut, 400:dict})
+def create_user(request, payload:UserCreate):
+    if User.objects.filter(username=payload.username).exists():
+        return Response({"detail": "Username already exists"}, status=400)
+    
+    user = User.objects.create_user(
+        username=payload.username,
+        email=payload.email,
+        password=payload.password,
+        first_name=payload.first_name,
+        last_name = payload.last_name
+    )
+
+    return Response(UserOut.from_orm(user), status=201)
--- pitlanebackend/settings.py
+++ pitlanebackend/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ninja'
+    'ninja',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  
 ]
 
 ROOT_URLCONF = 'pitlanebackend.urls'
